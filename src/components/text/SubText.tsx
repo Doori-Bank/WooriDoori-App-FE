0203c61393613bc8c01ae0d05ccaa137cf0945af
menzzi <minmin3927@naver.com>
2025-10-23 11:06:42 +0900

    [feat] #1 - 서브 텍스트 컴포넌트 생성

---
+++ src/components/text/SubText.tsx
@@ -0,0 +1,9 @@
+const SubText = ({ text }: { text: string }) => {
+  return (
+    <p className="text-[#858585] text-xl leading-relaxed">
+      {text}
+    </p>
+  );
+};
+
+export default SubText;
